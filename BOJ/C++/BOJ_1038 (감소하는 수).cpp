dbfe40ab4537b8a82e628899bd5806d237b44f26
hiwg08 <hiwg08@gmail.com>
2022-10-15 12:06:28 +0900

    2022.10/15

--- BOJ/C++/BOJ_1038 (감소하는 수).cpp
+++ BOJ/C++/BOJ_1038 (감소하는 수).cpp
@@ -2,50 +2,33 @@
 
 using namespace std;
 
-int N, Cnt = 0;
+int N, CHK = -1;
 
-vector<int> V;
-
-void Recurse(int Idx, int U)
+void Recurse(int Cnt, int Pre, string S) // 자릿수를 나타내는 Cnt, 현재 숫자인 Pre, 감소하는 수인 문자열 S
 {
-	if (Idx == 1)
+	if (Cnt == 0)
 	{
-		Cnt++;
-		V.push_back(U);
-
-		if (Cnt == N + 1)
+		if (++CHK == N) // 사용자가 입력한 번째의 수를 출력한 후 바로 프로그램 종료.
 		{
-			for (auto e : V)
-				cout << e;
-			cout << '\n';
+			cout << S << '\n';
 			exit(0);
 		}
-
-		V.pop_back();
 		return;
 	}
 
-	if (U == 0)
-		return;
+	if (Pre == -1)
+		return; // 감소시켰던 수가 음수면 성립되지 않으므로 리턴
 
-	for (int i = 0; i < U; i++)
-	{
-		V.push_back(U);
-		Recurse(Idx - 1, i);
-		V.pop_back();
-	}
+	for (int i = 0; i <= Pre; i++) // for문은 0부터 시작해야한다. 그래야 감소하는 수의 순서를 지킬 수 있다. ex. 10 -> "20" -> 21 -> "30" -> 31 -> 32 -> ...
+		Recurse(Cnt - 1, i - 1, S + to_string(i)); // 현재 있는 수(i) + 앞으로 감소할 수 (i - 1)
 }
 
 int main()
 {
 	cin >> N;
 
-	for (int k = 1; k <= 10; k++)
-	{
-		for (int i = k - 1; i <= 9; i++)
-			Recurse(k, i);
-	}
+	for (int i = 1; i <= 10; i++)
+		Recurse(i, 9, ""); // 10개의 자릿수만큼 반복
 
-	cout << -1 << '\n';
-
+	cout << -1 << '\n'; // 1024 이상(<= 1,000,000)의 수들은 모두 -1 출력.
 }
